088a5ae5a27748d82731722a2867b2fc95f86a72
FancyXGit <Fancy.fantasy688@gmail.com>
2025-10-03 16:06:50 +0800

    hw03

--- src/3 Tree Recursion/inverse_cascade.py
+++ src/3 Tree Recursion/inverse_cascade.py
@@ -1,14 +1,19 @@
-def inverse_cascade(n, k):
-    if k == 0:
-        print(n)
-        return
-    temp = n // pow(10, k)
-    print(temp)
-    inverse_cascade(n, k - 1)
-    print(temp)
+def inverse_cascade(n):
+    k = len(str(n)) - 1
+
+    def helper(n, k):
+        if k == 0:
+            print(n)
+            return
+        temp = n // pow(10, k)
+        print(temp)
+        helper(n, k - 1)
+        print(temp)
+
+    return helper(n, k)
 
 
-inverse_cascade(1234, 3)
+inverse_cascade(1234)
 
 print()
 
